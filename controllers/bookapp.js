f2a3e3bb7fc966859f6bf701efa106b2aa140e8e
howard <howard.ueckert@protonmail.com>
2025-01-21 21:37:48 -0600

    fixed css

--- controllers/bookapp.js
+++ controllers/bookapp.js
@@ -1,79 +1,116 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const mongoose = require("mongoose")
 
 const User = require('../models/user.js');
-
-router.get("/sign-in", (req,res) => {
-    res.render("auth/sign-in.ejs")
-})
-
-router.get("/sign-up", (req,res) => {
-    res.render("auth/sign-up.ejs")
-})
+const session = require('express-session');
 
 
-router.post("/sign-up", async (req,res) => {
+router.get('/', async (req, res) => {
     try {
-        const userExist = await User.findOne({ username: req.body.username });
-        
-        if (userExist) {
-          return res.send('Username already taken, please try another one.');
-        }
-
-        if (req.body.password !== req.body.confirmPassword) {
-          return res.send('Password and Confirm Password must match');
-        }
-      
-        //Hash the password before sending to the database
-        const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-        req.body.password = hashedPassword;
-      
-        // All ready to create the new user!
-        await User.create(req.body);
-      
-        res.redirect('/auth/sign-in');
-
+        const currentUser = await User.findById(req.session.user._id);
+        res.render('bookapp/index.ejs', {
+          books: currentUser.books,
+          user: req.session.user,
+        });
       } catch (error) {
+        console.log(error)
+        res.redirect('/')
+      }
+  });
+
+router.get('/new', async (req, res) => {
+    try {
+        res.render('bookapp/new.ejs', {
+          books: req.session.user.books,
+          user: req.session.user,
+        });
+      } catch (error) {
+        console.log(error)
+        res.redirect('/')
+      }
+  });
+
+router.post('/new', async(req,res) => {
+    try {
+        // Look up the user from req.session
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.books.push(req.body);
+        // Save changes to the user
+        await currentUser.save();
+        // Redirect back to the applications index view
+        res.redirect(`/users/${currentUser._id}/bookapp`);
+      } catch (error) {
+        // If any errors, log them and redirect back home
         console.log(error);
-        res.redirect('/');
+        res.redirect('/')
       }
 })
 
-router.post("/sign-in", async (req,res) => {
+router.get("/:bookId", async (req,res) => {
     try {
-        // First, get the user from the database
-        const userInDatabase = await User.findOne({ username: req.body.username });
-        if (!userInDatabase) {
-          return res.send('Login failed. Please try again.');
-        }
-      
-        // There is a user! Time to test their password with bcrypt
-        const validPassword = bcrypt.compareSync(
-          req.body.password,
-          userInDatabase.password
-        );
-        if (!validPassword) {
-          return res.send('Login failed. Please try again.');
-        }
-      
-        req.session.user = {
-          username: userInDatabase.username,
-          _id: userInDatabase._id
-        };
-      
-        res.redirect('/');
-      } catch (error) {
+        const currentUser = await User.findById(req.session.user._id);
+        const currentBook = currentUser.books.id(req.params.bookId)
+        res.render("bookapp/show.ejs", {
+            currentBook: currentBook,
+        });
+    } catch(error) {
         console.log(error);
-        res.redirect('/');
-      }
-
+        res.redirect('/')
+    }
 })
 
-router.get("/sign-out", (req,res) => {
-  req.session.destroy();
-  res.redirect("/")
+
+router.delete('/:bookId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const currentBook = currentUser.books.id(req.params.bookId)
+    //delete
+    currentBook.deleteOne();
+    //save
+    await currentUser.save();
+    //redirect
+    res.redirect(`/users/${currentUser._id}/bookapp`);
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect('/')
+  }
+});
+
+
+
+router.get("/:bookId/edit", async (req,res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const currentBook = currentUser.books.id(req.params.bookId)
+    res.render("bookapp/edit.ejs", {
+      currentBook: currentBook,
+    });
+    console.log(currentBook, currentUser)
+  } catch(error) {
+    console.log(error);
+    res.redirect('/')
+  }
 })
+
+
+router.put("/:bookId", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const currentBook = currentUser.books.id(req.params.bookId);
+    currentBook.set(req.body);
+    await currentUser.save();
+    res.redirect(
+      `/users/${currentUser._id}/bookapp/${req.params.bookId}`
+    );
+  } catch(error) {
+    console.log(error);
+    res.redirect('/')
+  }
+})
+
+
 
 module.exports = router;
-//! why do we export here
